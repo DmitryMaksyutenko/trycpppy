db666ed30af546c8553d7106dac3cd48bcf33a89
Dima-homeL <1988maksyutenko@gmail.com>
2021-02-16 16:24:10 +0200

    Author model modified.

--- ServerApp/roles/models.py
+++ ServerApp/roles/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Author(models.Model):
     """Model contains authors information."""
 
-    author_id = models.AutoField()
+    author_id = models.AutoField(primary_key=True)
     description = models.CharField(max_length=132, null=True)
     social = models.CharField(max_length=132, null=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
