97b9ba38d8f51cb61ce5e8d8a76a384b4ac582da
Dima-homeL <1988maksyutenko@gmail.com>
2021-02-16 15:28:50 +0200

    CreatedUpdatedFields abstract base model.

--- ServerApp/core/models.py
+++ ServerApp/core/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+
+
+class CreatedUpdatedFields(models.Model):
+    """Abstract base model with common timestamp fields."""
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=True)
+
+    class Meta:
+        abstract = True
