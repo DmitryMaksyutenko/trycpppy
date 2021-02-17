64ef8ac97a556bba5c6422ddcbc2ad734410acb1
Dima-homeL <1988maksyutenko@gmail.com>
2021-02-17 15:45:19 +0200

    Added Categories model.

--- ServerApp/blog/models.py
+++ ServerApp/blog/models.py
@@ -7,3 +7,9 @@ class Languages(CreatedUpdatedFields):
     """The table for programming languages."""
     language_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=32)
+
+
+class Categories(CreatedUpdatedFields):
+    """The table for categories of articles for the languages."""
+    category_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=56)
