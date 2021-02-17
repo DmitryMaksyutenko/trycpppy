39d8c41e72109b936ad8ca81a9fac034a0b96cda
Dima-homeL <1988maksyutenko@gmail.com>
2021-02-17 15:38:45 +0200

    Added Languages to admin.

--- ServerApp/blog/admin.py
+++ ServerApp/blog/admin.py
@@ -1,1 +1,13 @@
 from django.contrib import admin
+
+from .models import (
+    Languages
+)
+
+
+class LanguagesAdmin(admin.ModelAdmin):
+    """Custom settings."""
+    list_display = ("name",)
+
+
+admin.site.register(Languages, LanguagesAdmin)
