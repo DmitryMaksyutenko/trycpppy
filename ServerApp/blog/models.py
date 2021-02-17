5b40a1296926e7282ac86eea49a1567f7bde3339
Dima-homeL <1988maksyutenko@gmail.com>
2021-02-17 16:51:17 +0200

    Added Articles model.

--- ServerApp/blog/models.py
+++ ServerApp/blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.postgres.search import SearchVectorField
 
 from core.models import CreatedUpdatedFields
+from roles.models import Author
 
 
 class Languages(CreatedUpdatedFields):
@@ -20,3 +22,14 @@ class Categories(CreatedUpdatedFields):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Articles(CreatedUpdatedFields):
+    """The table for articles."""
+    article_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=56, null=True)
+    image = models.CharField(max_length=32, null=True)
+    content = SearchVectorField(null=True)
+    code = models.TextField(null=True)
+    category_id = models.ForeignKey(Categories, on_delete=models.RESTRICT)
+    author_id = models.ForeignKey(Author, on_delete=models.RESTRICT)
