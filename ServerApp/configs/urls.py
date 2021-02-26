a9bb270dd0eb372dde098f161c9ca7a3ff5a7181
Dima-homeL <1988maksyutenko@gmail.com>
2021-02-26 14:07:47 +0200

    admin ulr changed.

--- ServerApp/configs/urls.py
+++ ServerApp/configs/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from configs.settings.base import env
+
 
 urlpatterns = [
     path('grappelli/', include('grappelli.urls')),
-    path('admin/', admin.site.urls),
+    path(env("ADMIN_URL"), admin.site.urls),
 ]
