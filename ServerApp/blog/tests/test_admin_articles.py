a9cf39d31c3659356289c7c56c261cce0dd117a3
Dima-homeL <1988maksyutenko@gmail.com>
2021-03-17 11:48:57 +0200

    blog tests refactoring.

--- ServerApp/blog/tests/test_admin_articles.py
+++ ServerApp/blog/tests/test_admin_articles.py
@@ -11,15 +11,16 @@ from django.contrib.admin.sites import AdminSite
 from configs.settings.base import env
 from blog.admin import ArticlesAdmin
 from blog.models import (
-    Articles,
-    Authors,
-    Languages,
-    Categories,
-    CategoriesLanguages
+    Articles, Authors, Languages,
+    Categories, CategoriesLanguages
+)
+from core.tests.definitions import (
+    BASE_TEST_URL, TEST_IMG_NAME, TEST_USERNAME,
+    TEST_PASS, TEST_EMAIL
 )
 
-IMG_NAME = "test.jpg"
-URL = "http://testserver/" + env("ADMIN_URL") + "/articles/"
+URL = BASE_TEST_URL + env("ADMIN_URL") + "/articles/"
+LOG_PATH = env("LOG_DIR") + "/articles.log"
 
 
 class TestArticlesAdmin(TestCase):
@@ -27,85 +28,89 @@ class TestArticlesAdmin(TestCase):
         self.site = AdminSite()
         self.ma = ArticlesAdmin(Articles, self.site)
         self.img = mock.MagicMock(spec=ImageFile, name="img_mock")
-        self.img.name = IMG_NAME
+        self.img.name = TEST_IMG_NAME
         self.IMG_PATH = settings.MEDIA_ROOT + self.img.name
         User.objects.create_user(
             id=1,
-            username="Testname",
-            password="0000",
-            email="test@mail.com"
+            username=TEST_USERNAME,
+            password=TEST_PASS,
+            email=TEST_EMAIL
         )
-        Authors.objects.create(
+        self.grup = Authors.objects.create(
             user_id=User.objects.get(pk=1),
             author_id=1
         )
-        Categories.objects.create(
+        self.categry = Categories.objects.create(
             category_id=1,
             name="test"
         )
-        Languages.objects.create(
+        self.language = Languages.objects.create(
             language_id=1,
             name="Python"
         )
-        CategoriesLanguages.objects.create(
+        self.cl = CategoriesLanguages.objects.create(
             id=1,
             language_id=Languages.objects.get(pk=1),
             category_id=Categories.objects.get(pk=1)
         )
-        Articles.objects.create(
+        self.article = Articles.objects.create(
             article_id=1,
             title="test",
             author_id=1,
             image=self.img,
-            category=CategoriesLanguages.objects.get(pk=1)
+            category=self.cl
         )
 
     def test_save_new_article(self):
-        log_size_before = os.stat(env("LOG_DIR") + "/articles.log").st_size
-        self.ma.save_model(
-            self.client.post(URL + "add/", {}),
-            Articles.objects.create(
+        """Testing, when createing a new article, the new
+           log racord about that action is adds to teh log file.
+        """
+        log_size_before = os.stat(LOG_PATH).st_size
+        new_article = Articles.objects.create(
                 article_id=2,
                 title="Test",
                 author_id=1,
                 image=self.img,
-                category=CategoriesLanguages.objects.get(pk=1)
-            ),
+                category=self.cl
+            )
+        self.ma.save_model(
+            self.client.post(URL + "add/", {}),
+            new_article,
             {},
             False
         )
-        log_size_after = os.stat(env("LOG_DIR") + "/articles.log").st_size
+        log_size_after = os.stat(LOG_PATH).st_size
         self.assertNotEqual(log_size_before, log_size_after)
-        os.remove(Articles.objects.get(pk=1).image.path)
-        os.remove(Articles.objects.get(pk=2).image.path)
+        os.remove(self.article.image.path)
+        os.remove(new_article.image.path)
 
     def test_change_article(self):
-        log_size_before = os.stat(env("LOG_DIR") + "/articles.log").st_size
-        article = Articles.objects.get(pk=1)
-        article.title = "new"
+        """Testing, when change an article, the new
+           log racord about that action is adds to teh log file.
+        """
+        log_size_before = os.stat(LOG_PATH).st_size
+        self.article.title = "new"
         self.ma.save_model(
             self.client.post(URL + "1/change/", {}),
-            article,
+            self.article,
             {},
             True
         )
-        log_size_after = os.stat(env("LOG_DIR") + "/articles.log").st_size
+        log_size_after = os.stat(LOG_PATH).st_size
         self.assertNotEqual(log_size_before, log_size_after)
-        os.remove(Articles.objects.get(pk=1).image.path)
+        os.remove(self.article.image.path)
 
     def test_article_deletion(self):
-        image = Path(Articles.objects.get(pk=1).image.path)
-        article = Articles.objects.get(pk=1)
-        os.remove(Articles.objects.get(pk=1).image.path)
-        self.ma.delete_model(
-            self.client.get(URL + "1/delete/"),
-            article
-        )
+        """Testing, when delete an article, the image must be removed."""
+        image = Path(self.article.image.path)
+        os.remove(self.article.image.path)
+        self.ma.delete_model(self.client.get(URL + "1/delete/"), self.article)
         self.assertFalse(image.exists())
 
     def test_deleteion_articles_set(self):
+        """Testing, deleton an articles set."""
         articles = Articles.objects.all()
-        os.remove(Articles.objects.get(pk=1).image.path)
+        os.remove(self.article.image.path)
         self.ma.delete_queryset(
             self.client.post(URL, {}),
             articles
--- ServerApp/blog/tests/test_models.py
+++ ServerApp/blog/tests/test_models.py
@@ -9,45 +9,44 @@ from django.contrib.auth.models import User
 from django.core.files.images import ImageFile
 
 from blog.models import (
-    Articles,
-    Authors,
-    Languages,
-    Categories,
-    CategoriesLanguages
+    Articles, Authors, Languages,
+    Categories, CategoriesLanguages
 )
-
-IMG_NAME = "test.jpg"
+from core.tests.definitions import (
+    TEST_USERNAME, TEST_PASS, TEST_EMAIL,
+    TEST_IMG_NAME
+)
 
 
 class TestArticles(TestCase):
     def setUp(self) -> None:
         self.img = mock.MagicMock(spec=ImageFile, name="img_mock")
-        self.img.name = IMG_NAME
+        self.img.name = TEST_IMG_NAME
         self.IMG_PATH = settings.MEDIA_ROOT + self.img.name
         User.objects.create_user(
             id=1,
-            username="Testname",
-            password="0000",
-            email="test@mail.com"
+            username=TEST_USERNAME,
+            password=TEST_PASS,
+            email=TEST_EMAIL
         )
-        Authors.objects.create(
+        self.author = Authors.objects.create(
             user_id=User.objects.get(pk=1),
             author_id=1
         )
-        Categories.objects.create(
+        self.category = Categories.objects.create(
             category_id=1,
             name="test"
         )
-        Languages.objects.create(
+        self.language = Languages.objects.create(
             language_id=1,
             name="Python"
         )
-        CategoriesLanguages.objects.create(
+        self.cl = CategoriesLanguages.objects.create(
             id=1,
             language_id=Languages.objects.get(pk=1),
             category_id=Categories.objects.get(pk=1)
         )
-        Articles.objects.create(
+        self.article = Articles.objects.create(
             article_id=1,
             title="test",
             author_id=1,
@@ -59,42 +58,50 @@ class TestArticles(TestCase):
         os.remove(self.IMG_PATH)
 
     def test_artileces_equal(self):
+        """Testing the __eq__ method."""
         curr = Articles.objects.get(pk=1)
         new = Articles.objects.get(pk=1)
         self.assertEqual(curr, new)
 
     def test_articles_not_equal(self):
-        curr = Articles.objects.get(pk=1)
+        """Testing the __ne__ method."""
         new = Articles(
             article_id=2,
             title="new test",
             author_id=1,
             image=self.img,
-            category=CategoriesLanguages.objects.get(pk=1)
+            category=self.cl
         )
         new.save()
         os.remove(new.image.path)
-        self.assertNotEqual(curr, new)
+        self.assertNotEqual(self.article, new)
 
     def test_articles_user_update(self):
-        curr = Articles.objects.get(pk=1)
+        """Testing, updating the existing article."""
         Articles.objects.filter(pk=1).update(title="new test")
         new = Articles.objects.get(pk=1)
-        self.assertNotEqual(curr, new)
+        self.assertNotEqual(self.article, new)
 
     def test_no_content_and_content_vector(self):
-        article = Articles.objects.get(pk=1)
-        self.assertFalse(article.content)
-        self.assertFalse(article.content_vector)
+        """Testing, if no content supplied,
+            then content_vector is empty too.
+        """
+        self.assertFalse(self.article.content)
+        self.assertFalse(self.article.content_vector)
 
     def test_add_content(self):
-        article = Articles.objects.get(pk=1)
-        article.content = "Test content."
-        article.save()
-        self.assertTrue(article.content)
-        self.assertTrue(article.content_vector)
+        """Testing, if content supplied,
+            then content_vector is not empty.
+        """
+        self.article.content = "Test content."
+        self.article.save()
+        self.assertTrue(self.article.content)
+        self.assertTrue(self.article.content_vector)
 
     def test_delete_article_image(self):
+        """Testing, the image must be removed from the media
+           directory when an article is deleted.
+        """
         article = Articles.objects.create(
             article_id=2,
             title="test",
@@ -108,12 +115,14 @@ class TestArticles(TestCase):
         self.assertFalse(img_dir.exists())
 
     def test_repr_str(self):
-        self.assertEqual(str(Articles.objects.get(pk=1)), "test")
+        """Testing the Articles representation string."""
+        self.assertEqual(str(self.article), "test")
 
 
 class TestCategories(TestCase):
 
     def test_repr_categories(self):
+        """Testing the Categories representation string."""
         category = Categories.objects.create(
             category_id=1,
             name="Test category",
@@ -124,6 +133,7 @@ class TestCategories(TestCase):
 class TestLanguages(TestCase):
 
     def test_repr_lanuages(self):
+        """Testing the Languages representation string."""
         language = Languages.objects.create(
             language_id=1,
             name="Python",
